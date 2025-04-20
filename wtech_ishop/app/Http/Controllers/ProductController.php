4b86a18b78ebecbc06277d09d7c5ba2a7fb470e5
picklemaster <branis161@gmail.com>
2025-04-20 21:03:32 +0200

    updated product detail

--- wtech_ishop/app/Http/Controllers/ProductController.php
+++ wtech_ishop/app/Http/Controllers/ProductController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Category;
 
 class ProductController extends Controller
 {
     public function detail($id)
     {
+        $product = Product::findOrFail($id);
+        $categories = Category::all();
+
+        $category = Category::findOrFail($product->category_id);
+
         $product = Product::with('images')->findOrFail($id);
-        return view('pages.detail', compact('product'));
+        return view('pages.detail', compact('product', 'category', 'categories'));
     }
 }
--- wtech_ishop/resources/views/pages/detail.blade.php
+++ wtech_ishop/resources/views/pages/detail.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'iSHOP - dynamicky_detail')
+@section('title', 'iSHOP - ' . ($category->category ?? 'Detail'))
 
 @section('page-css')
     <link rel="stylesheet" href="{{ asset('pages/detail/styles.css') }}">
@@ -18,31 +18,14 @@
         <!-- Sidebar -->
         <nav id="sidebarMenu" class="col-lg-2 collapse d-md-block p-3" style="background-color: #E5EBEA;">
             <ul class="nav flex-column">
-                <li class="nav-item">
-                    <a class="nav-link sidebar-category rounded-pill" style="color: #45503B;" href="{{ route('category') }}">
-                        <i class="bi bi-laptop"></i> Kategoria 1
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link sidebar-category rounded-pill" style="color: #45503B;" href="../category/category.html">
-                        <i class="bi bi-phone"></i> Kategoria 2
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link sidebar-category rounded-pill" style="color: #45503B;" href="../category/category.html">
-                        <i class="bi bi-headphones"></i> Kategoria 3
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link sidebar-category rounded-pill" style="color: #45503B;" href="../category/category.html">
-                        <i class="bi bi-tv"></i> Kategoria 4
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link sidebar-category rounded-pill" style="color: #45503B;" href="../category/category.html">
-                        <i class="bi bi-usb-symbol"></i> Kategoria 5
-                    </a>
-                </li>
+                @foreach ($categories as $category)
+                    <li class="nav-item">
+                        <a class="nav-link sidebar-category rounded-pill" style="color: #45503B;"
+                            href="{{ route('category', $category->slug) }}">
+                            <i class="bi bi-{{ $category->icon }}"></i> {{ $category->category }}
+                        </a>
+                    </li>
+                @endforeach
             </ul>
         </nav>
 
@@ -63,36 +46,14 @@
                         </div>
 
                         <div class="carousel-inner ratio ratio-1x1 bg-white">
-                            <div class="carousel-item active h-100">
-                                <div class="d-flex justify-content-center align-items-center h-100">
-                                    <img src="../../assets/iphone/iphone_pro_black_1.webp"
-                                        class="d-block img-fluid mh-100 object-fit-contain" alt="Product Image 1">
+                            @foreach ($product->images as $index => $image)
+                                <div class="carousel-item {{ $index === 0 ? 'active' : '' }} h-100">
+                                    <div class="d-flex justify-content-center align-items-center h-100">
+                                        <img src="{{ asset( $image->path) }}"
+                                            class="d-block img-fluid mh-100 object-fit-contain" alt="Product Image {{ $index + 1 }}">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="carousel-item h-100">
-                                <div class="d-flex justify-content-center align-items-center h-100">
-                                    <img src="../../assets/iphone/iphone_pro_black_2.webp"
-                                        class="d-block img-fluid mh-100 object-fit-contain" alt="Product Image 2">
-                                </div>
-                            </div>
-                            <div class="carousel-item h-100">
-                                <div class="d-flex justify-content-center align-items-center h-100">
-                                    <img src="../../assets/iphone/iphone_pro_black_3.webp"
-                                        class="d-block img-fluid mh-100 object-fit-contain" alt="Product Image 3">
-                                </div>
-                            </div>
-                            <div class="carousel-item h-100">
-                                <div class="d-flex justify-content-center align-items-center h-100">
-                                    <img src="../../assets/iphone/iphone_pro_black_4.webp"
-                                        class="d-block img-fluid mh-100 object-fit-contain" alt="Product Image 4">
-                                </div>
-                            </div>
-                            <div class="carousel-item h-100">
-                                <div class="d-flex justify-content-center align-items-center h-100">
-                                    <img src="../../assets/iphone/iphone_pro_black_5.webp"
-                                        class="d-block img-fluid mh-100 object-fit-contain" alt="Product Image 5">
-                                </div>
-                            </div>
+                            @endforeach
                         </div>
 
                         <button class="carousel-control-prev" type="button" data-bs-target="#imageGallery"
@@ -111,30 +72,46 @@
                 <!-- Product Details Column -->
                 <div class="col-md-6 d-flex justify-content-center">
                     <div class="w-75">
-                        <h3 class="fw-bold ms-3 mb-5 mt-4 text-color">Iphone 16 Pro - cierna</h3>
-                        <p class="product-text text-color">Mobilný telefón - 6,3" Super Retina XDR OLED 2622 x 1206 (120 Hz),
-                            operačná pamäť 8 GB, vnútorná pamäť 256 GB, single SIM + eSIM, procesor Apple A18 Pro,
-                            fotoaparát: 48 Mpx (f/1,78) hlavný + 48 Mpx širokouhlý + 12 Mpx teleobjektív, predná kamera
-                            12 Mpx, GPS, NFC, LTE, 5G, USB-C, vodoodolný podľa IP68, rýchle nabíjanie, bezdrôtové
-                            nabíjanie 25 W, batéria 3582 mAh, model 2024, iOS</p>
+                        <h3 class="fw-bold ms-3 mb-5 mt-4 text-color">{{ $product->name }}</h3>
+
+                        <p class="product-text text-color">{{ $product->description }}</p>
 
                         <div class="ms-3 d-flex align-items-center mt-3">
-                            <input type="number" class="form-control me-3 rounded-pill" value="1" min="1" max="99"
+                            <input id="quantityInput" type="number" class="form-control me-3 rounded-pill" value="1" min="1" max="{{ $product->stockquantity }}"
                                 style="width: 60px; text-align: left;">
+                                <script>
+                                    const input = document.getElementById('quantityInput');
+                                    input.addEventListener('input', function () {
+                                        const max = parseInt(input.max);
+                                        const val = parseInt(input.value);
+                                        if (val > max) {
+                                            input.value = max;
+                                        }
+                                    });
+                                </script>
+
                             <button class="btn btn-primary rounded-pill button_color" style="min-width: 145px;">
                                 Pridať do košíka
                             </button>
 
-                            <span class="ms-2 product-uid"
-                                style="opacity: 0.4; font-size: 0.8rem; color: #6c757d;">AS969AE</span>
-
-                            <button class="btn btn-outline-secondary"
-                                style="opacity: 0.4; border: none; background: none;">
-                                <i class="bi bi-pencil" style="font-size: 1.2rem; color: #6c757d;"></i>
-                            </button>
+                            
+                            @php
+                            $user = session('user');
+                            @endphp
+                            
+                            @if ($user && $user->role === 'admin')
+                                    <span class="ms-2 product-uid"
+                                        style="opacity: 0.4; font-size: 0.8rem; color: #6c757d;">{{ $product->id }}</span>
+                                        
+                                    <button class="btn btn-outline-secondary"
+                                        style="opacity: 0.4; border: none; background: none;">
+                                        <i class="bi bi-pencil" style="font-size: 1.2rem; color: #6c757d;"></i>
+                                    </button>
+                                @endif
                         </div>
                     </div>
                 </div>
+
             </div>
     </div>
     </main>
